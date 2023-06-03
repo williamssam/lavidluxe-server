db8396526a0ff6afdbcb871651da6fff59f4c662
Williams Samuel <samwill300@gmail.com>
2023-06-03 21:18:52 +0100

    loads of changes...

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,6 @@
 import category from './category'
-import faqs from './faqs'
 import order from './order'
 import orderItem from './orderItem'
 import product from './product'
 
-export const schemaTypes = [product, category, order, orderItem, faqs]
+export const schemaTypes = [product, category, order, orderItem]
